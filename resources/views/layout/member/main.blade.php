43d3de63a6f6cbc876573ebd4dba9a3c4c74902b
Chairil <chairilptmgahama@gmail.com>
2019-09-03 10:45:12 +0700

    ganti meta

--- resources/views/layout/member/main.blade.php
+++ resources/views/layout/member/main.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Safra">
-        <meta name="author" content="safra IT">
+        <meta name="description" content="Lumbung Network">
+        <meta name="author" content="Lumbung Network IT">
         <link rel="shortcut icon" href="/image/icon2.png">
         <title>Lumbung Network</title>
         <link href="{{ asset('asset_member/css/style.css') }}" rel="stylesheet" type="text/css" />
