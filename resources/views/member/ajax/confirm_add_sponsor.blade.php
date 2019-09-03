00f7e1713c4986837bf0cef654a30be37ffaf74f
Chairil <chairilptmgahama@gmail.com>
2019-09-03 18:34:03 +0700

    nama ga pake

--- resources/views/member/ajax/confirm_add_sponsor.blade.php
+++ resources/views/member/ajax/confirm_add_sponsor.blade.php
@@ -8,32 +8,24 @@
                 <form id="form-add" method="POST" action="/m/add/sponsor">
                     {{ csrf_field() }}
                 <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" class="form-control" readonly="" name="name" value="{{$dataRequest->name}}">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" class="form-control" readonly="" name="user_code" value="{{$dataRequest->username}}">
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-8">
                         <div class="form-group">
                             <label>Email</label>
                             <input type="text" class="form-control" readonly="" name="email" value="{{$dataRequest->email}}">
                         </div>
                     </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Phone</label>
+                            <input type="text" class="form-control" readonly="" name="hp" value="{{$dataRequest->hp}}">
+                        </div>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Phone</label>
-                            <input type="text" class="form-control" readonly="" name="hp" value="{{$dataRequest->hp}}">
+                            <label>Username</label>
+                            <input type="text" class="form-control" readonly="" name="user_code" value="{{$dataRequest->username}}">
                         </div>
                     </div>
                     <div class="col-md-6">
