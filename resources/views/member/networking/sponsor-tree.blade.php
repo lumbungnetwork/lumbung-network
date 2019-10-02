193c4274a55e2c08cfdd8623026ade97d6e8796e
Chairil <chairilptmgahama@gmail.com>
2019-10-02 17:16:20 +0700

    tree sponsor baru search dl. klik blm, lg diluar

--- resources/views/member/networking/sponsor-tree.blade.php
+++ resources/views/member/networking/sponsor-tree.blade.php
@@ -17,13 +17,33 @@
                 <div class="col-xs-12">
                     <div class="card-box">
                         @if ( Session::has('message') )
-                        <div class="alert alert-{{ Session::get('messageclass') }} alert-dismissible fade in" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                            </button>
-                            {{  Session::get('message')    }} 
-                        </div>
+                            <div class="alert alert-{{ Session::get('messageclass') }} alert-dismissible fade in" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                                {{  Session::get('message')    }} 
+                            </div>
                         @endif
+                        <form class="login100-form validate-form" method="get" action="/m/my/sponsor-tree">
+                        {{ csrf_field() }}
+                        <div class="row">
+                            <div class="col-xs-12 col-xs-12">
+                                <fieldset class="form-group">
+                                    <label>Cari Member (By Username)</label>
+                                    <input type="text" class="form-control" id="get_id" autocomplete="off">
+                                    <input type="hidden" name="get_id" id="id_get_id">
+                                    <ul class="typeahead dropdown-menu form-control" style="max-height: 120px; overflow: auto;border: 1px solid #ddd;width: 98%;margin-left: 11px;" id="get_id-box"></ul>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <button type="submit" class="btn btn-primary" >Cari</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                    <div class="card-box">
                         <div class="row">
                             <div class="col-xs-12">
                                 <div class="region region-content">
