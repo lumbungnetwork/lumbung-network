c3a06eb1a19ec7c8248d7619c76ee2162fb1efb2
Chairil <chairilptmgahama@gmail.com>
2020-01-31 15:30:00 +0700

    bug reward belanja

--- resources/views/member/bonus/req-belanja-reward.blade.php
+++ resources/views/member/bonus/req-belanja-reward.blade.php
@@ -48,11 +48,15 @@
                                         @foreach($getData as $row)
                                             <?php
                                                 $kelipatan = floor(($row->month_sale_price/10000)/10) * 10;
+                                                $reward = '--';
+                                                if($kelipatan > 0){
+                                                    $reward = $kelipatan.' LMB';
+                                                }
                                             ?>
                                             <tr>
                                                 <td>{{$row->monthly}}</td>
                                                 <td>{{number_format($row->month_sale_price, 0, ',', '.')}}</td>
-                                                <td>{{$kelipatan}} LMB</td>
+                                                <td>{{$reward}}</td>
                                                 <td>
                                                     @if($row->canClaim == 1)
                                                     <a rel="tooltip" data-toggle="modal" data-target="#confirmSubmit" class="btn btn-custom btn-sm waves-effect waves-light" href="{{ URL::to('/') }}/m/cek/confirm-belanja-reward?m={{$row->month}}&y={{$row->year}}">
