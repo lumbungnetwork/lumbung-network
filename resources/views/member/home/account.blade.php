d443937a9e1ac7296f234ff6a84ba23ad75835d5
Chairil Hakim <chairilptmgahama@gmail.com>
2020-06-02 20:39:50 +0700

    disable tombol

--- resources/views/member/home/account.blade.php
+++ resources/views/member/home/account.blade.php
@@ -241,10 +241,12 @@
                     </div>
                     @endif
                     @if($dataUser->is_stockist == 1)
-                    <a href="{{ URL::to('/') }}/m/stockist/penjualan-reward" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a>
+                    <?php //<a href="{{ URL::to('/') }}/m/stockist/penjualan-reward" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a> ?>
+                    <a href="#" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a>
                     @endif
                     @if($dataUser->is_vendor == 1)
-                    <a href="{{ URL::to('/') }}/m/vendor/penjualan-reward" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a>
+                    <?php //<a href="{{ URL::to('/') }}/m/vendor/penjualan-reward" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a> ?>
+                    <a href="#" class="btn btn-warning btn-block shadow-sm mb-3">  <div class="f-14">Claim Reward Penjualan  </div></a>
                     @endif
                     
                 </div>
