d53f8dc814a11aec7781c5b2bb162796a651ff58
Chairil <chairil@ioc.sc>
2020-09-19 17:54:19 +0700

    bug pendign reject

--- resources/views/member/digital/m_detail_vppob.blade.php
+++ resources/views/member/digital/m_detail_vppob.blade.php
@@ -110,14 +110,16 @@
                                         @endif
                                         @if($getDataMaster->status == 1)
                                             <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
-                                            <button type="submit" class="btn btn-danger"  id="submitBtn" data-toggle="modal" data-target="#rejectSubmit" onClick="rejectSubmit()">Batal</button>
-                                            &nbsp;
-                                            <form method="POST" action="/m/confirm/vppob" style="display: contents;">
-                                                {{ csrf_field() }}
-                                                <input type="hidden" name="ppob_id" value="{{$getDataMaster->id}}">
-                                                <input type="hidden" name="harga_modal" value="{{$getDataMaster->harga_modal}}">
-                                                <button type="submit" class="btn btn-success" id="submitBtn">Konfirmasi</button>
-                                            </form>
+                                            @if($getDataMaster->vendor_cek == null)
+                                                <button type="submit" class="btn btn-danger"  id="submitBtn" data-toggle="modal" data-target="#rejectSubmit" onClick="rejectSubmit()">Batal</button>
+                                                &nbsp;
+                                                <form method="POST" action="/m/confirm/vppob" style="display: contents;">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="ppob_id" value="{{$getDataMaster->id}}">
+                                                    <input type="hidden" name="harga_modal" value="{{$getDataMaster->harga_modal}}">
+                                                    <button type="submit" class="btn btn-success" id="submitBtn">Konfirmasi</button>
+                                                </form>
+                                            @endif
                                         @endif
                                         @if($getDataMaster->status == 2)
                                             <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
