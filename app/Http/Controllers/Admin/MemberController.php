a9f7192ddcc5a6eda94da0dd35bc2beead85abd9
Evan Stinger <evanstinger@icloud.com>
2020-11-16 11:40:15 +0700

    Furter reducing PPOB confirm Retries to 14

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -5376,7 +5376,7 @@ class MemberController extends Controller
             $arrayData = json_decode($cek, true);
 
             $i++;
-            if ($i > 18) goto end;
+            if ($i > 14) goto end;
             if ($arrayData == null) {
                 return redirect()->back()
                     ->with('message', 'Ada gangguan koneksi, periksa jaringan internet anda, lalu ulangi kembali.')
