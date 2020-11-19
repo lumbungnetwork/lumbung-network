2750cefb6e757345029c155a3c0ac501d37e4690
Evan Stinger <evanstinger@icloud.com>
2020-11-19 22:13:16 +0700

    Tidying up seller's view on Input Stock

--- resources/views/member/sales/stockist_input_stock.blade.php
+++ resources/views/member/sales/stockist_input_stock.blade.php
@@ -17,45 +17,54 @@
         </div>
         <div class="mt-min-10">
             <div class="container">
-                
+
                 @if($getData != null)
-                <div class="rounded-lg bg-white p-3 mb-3">
-                    <h6 class="mb-3">Input Stock Vendor</h6>
+                <div class="rounded-lg p-0 mb-3">
+                    <h6 class="mb-3">Input Stock</h6>
                     @if ( Session::has('message') )
-                        <div class="alert alert-{{ Session::get('messageclass') }} alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                            {{  Session::get('message')    }} 
-                        </div>
+                    <div class="alert alert-{{ Session::get('messageclass') }} alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                        {{  Session::get('message')    }}
+                    </div>
                     @endif
                     <div class="row">
                         @foreach($getData as $row)
-                        <div class="col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 20px;text-align: center;">
-                            <div class="sc-product-item thumbnail card card-block">
-                                <div class="caption">
-                                    <img data-name="product_image" src="{{$row->image}}" alt="..." style="width: 150px;">
-                                    <h5 data-name="product_name">{{$row->name}} </h5>
-                                    <h6 data-name="product_desc">{{$row->ukuran}} </h6>
-                                    <?php //<h5 data-name="product_name"><b>Rp. {{number_format($row->stockist_price, 0, ',', ',')}}</b></h5> ?>
-                                    <h5 data-name="product_name"><b>Rp. {{number_format($row->member_price, 0, ',', ',')}}</b></h5>
+                        <div class="col-6 p-2" style="margin-bottom: 20px;text-align: center;">
+                            <div class="sc-product-item thumbnail rounded-lg bg-white shadow p-2 px-0">
+
+                                <div style="line-height: 5px;" class="caption">
+                                    <img data-name="product_image" src="{{$row->image}}" alt="{{$row->name}}"
+                                        style="width: auto; max-width: 100%;">
+                                    <h6 style="font-size: 14px; font-weight:200;" data-name="product_name">
+                                        {{$row->name}} </h6>
+                                    <dd style="font-size:12px" data-name="product_desc">{{$row->ukuran}} </dd>
+                                    <dd style="font-size: 15px; margin-top: 15px;">
+                                        <b>Rp{{number_format($row->member_price, 0, ',', ',')}}</b>
+                                    </dd>
+
                                     <div>
                                         <div class="form-group2">
-                                            <input class="sc-cart-item-qty" name="product_quantity" min="1" value="1" type="number">
+                                            <input class="sc-cart-item-qty" name="product_quantity" min="1" value="1"
+                                                type="number">
                                         </div>
-                                        <?php //<input name="product_price" value="{{number_format($row->stockist_price, 0, ',', '')}}" type="hidden" /> ?>
-                                        <input name="product_price" value="{{number_format($row->member_price, 0, ',', '')}}" type="hidden" />
+                                        <input name="product_price"
+                                            value="{{number_format($row->member_price, 0, ',', '')}}" type="hidden" />
                                         <input name="product_id" value="{{$row->id}}" type="hidden" />
-                                        <button class="sc-add-to-cart btn btn-success btn-sm  margin-tb-10">Masuk ke keranjang</button>
+                                        <button class="sc-add-to-cart btn btn-success btn-sm margin-tb-10"> <span
+                                                style="font-size: 15px;">+</span> ke Keranjang</button>
                                     </div>
-                                    <div class="clearfix"></div>
                                 </div>
+
+                                <div class="clearfix"></div>
                             </div>
                         </div>
+
                         @endforeach
                     </div>
                 </div>
-                
+
                 <div class="rounded-lg bg-white p-3 mb-3">
                     <div class="row">
                         <div class="col-xl-12 col-xs-12">
@@ -69,23 +78,23 @@
                     </div>
                 </div>
                 @endif
-                
+
                 @if($getData == null)
                 <div class="rounded-lg bg-white p-3 mb-3">
                     <div class="row">
-                        
+
                         <div class="col-xs-12 col-md-6 col-lg-6 col-xl-12">
                             <div class="card-box tilebox-one">
-                                    <div class="alert alert-warning alert-dismissible" role="alert">
-                                        Tidak ada data stock
-                                    </div>
+                                <div class="alert alert-warning alert-dismissible" role="alert">
+                                    Tidak ada data stock
+                                </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
                 @endif
-                
+
             </div>
         </div>
         @include('layout.member.nav')
@@ -98,21 +107,25 @@
 
 @section('styles')
 <link rel="stylesheet" href="{{ asset('asset_member/css/cart.css') }}">
-    <link rel="stylesheet" href="{{ asset('asset_new/css/siderbar.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.9.95/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/fonts/slick.woff">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-    
+<link rel="stylesheet" href="{{ asset('asset_new/css/siderbar.css') }}">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.9.95/css/materialdesignicons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/fonts/slick.woff">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
 @stop
 
 @section('javascript')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="{{ asset('asset_new/js/sidebar.js') }}"></script>
-    <script src="{{ asset('asset_member/js/jquery.cart.min.js') }}"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js">
+</script>
+<script src="{{ asset('asset_new/js/sidebar.js') }}"></script>
+<script src="{{ asset('asset_member/js/jquery.cart.min.js') }}"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
             $('#smartcart').smartCart();
         });
 </script>
