fc38ad5c472d5787b4d88d99aae89836ae8ae898
Evan Stinger <evanstinger@icloud.com>
2020-11-22 17:17:39 +0700

    Upgrade to Laravel 7.29.3

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Middleware;
 
+use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated {
-
-    public function handle($request, Closure $next, $guard = null) {
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
         if (Auth::guard($guard)->check()) {
-//            return redirect('/home');
+            // return redirect(RouteServiceProvider::HOME);
             return $next($request);
         }
         return redirect()->route('areaLogin');
-//        return $next($request);
+        //        return $next($request);
     }
 }
