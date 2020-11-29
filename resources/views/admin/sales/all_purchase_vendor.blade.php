727c4242b8472a0fe3e6e0c77be536f28a59eb4b
Evan Stinger <evanstinger@icloud.com>
2020-11-30 00:09:27 +0700

    Enable search on product lists

--- resources/views/admin/sales/all_purchase_vendor.blade.php
+++ resources/views/admin/sales/all_purchase_vendor.blade.php
@@ -2,23 +2,23 @@
 @section('content')
 @include('layout.admin.sidebar')
 <div class="main-panel">
-    
+
     <?php //MENU HEADER  ?>
     <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
         <div class="container-fluid">
             <div class="navbar-wrapper">
                 <div class="navbar-toggle">
                     <button type="button" class="navbar-toggler">
-                    <span class="navbar-toggler-bar bar1"></span>
-                    <span class="navbar-toggler-bar bar2"></span>
-                    <span class="navbar-toggler-bar bar3"></span>
+                        <span class="navbar-toggler-bar bar1"></span>
+                        <span class="navbar-toggler-bar bar2"></span>
+                        <span class="navbar-toggler-bar bar3"></span>
                     </button>
                 </div>
                 <p class="navbar-brand">{{$headerTitle}}</p>
             </div>
         </div>
     </nav>
-    
+
     <?php //MENU CONTENT  ?>
     <div class="content">
         <div class="row">
@@ -26,18 +26,20 @@
                 <div class="card">
                     <div class="card-header">
                         <h5 class="card-title">List</h5>
-                        <a class="btn btn-info btn-fill btn-sm" href="{{ URL::to('/') }}/adm/add/vpurchase">Tambah Produk Vendor</a>
+                        <a class="btn btn-info btn-fill btn-sm" href="{{ URL::to('/') }}/adm/add/vpurchase">Tambah
+                            Produk Vendor</a>
                     </div>
                     <div class="card-body">
                         @if ( Session::has('message') )
-                            <div class="widget-content mt10 mb10 mr15">
-                                <div class="alert alert-{{ Session::get('messageclass') }}">
-                                    <button class="close" type="button" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
-                                    {{  Session::get('message')    }} 
-                                </div>
+                        <div class="widget-content mt10 mb10 mr15">
+                            <div class="alert alert-{{ Session::get('messageclass') }}">
+                                <button class="close" type="button" data-dismiss="alert"><span
+                                        aria-hidden="true">&times;</span></button>
+                                {{  Session::get('message')    }}
                             </div>
+                        </div>
                         @endif
-                         <div class="table-responsive">
+                        <div class="table-responsive">
                             <table class="table table-striped nowrap" id="myTable">
                                 <thead class=" text-primary">
                                     <tr>
@@ -51,35 +53,39 @@
                                         <th>###</th>
                                     </tr>
                                 </thead>
-                                
+
                                 <tbody>
                                     @if($getData != null)
-                                        <?php 
-                                        $no = 0; 
+                                    <?php
+                                        $no = 0;
                                         ?>
-                                        @foreach($getData as $row)
-                                        <?php 
+                                    @foreach($getData as $row)
+                                    <?php
                                             $no++;
                                         ?>
-                                            <tr>
-                                                <td>{{$no}}</td>
-                                                <td>{{$row->name}}</td>
-                                                <td>{{$row->ukuran}}</td>
-                                                <!--<td>{{number_format($row->stockist_price, 0, ',', ',')}}</td>-->
-                                                <td>{{number_format($row->member_price, 0, ',', ',')}}</td>
-                                                <td>{{$row->code}}</td>
-                                                <td>{{$row->area}}</td>
-                                                <td>
-                                                    <a class="text-info" href="{{ URL::to('/') }}/adm/edit/vpurchase/{{$row->id}}">edit</a>
-                                                    &nbsp;&nbsp;
-                                                     <a rel="tooltip"  data-toggle="modal" data-target="#rmThis" class="text-danger" href="{{ URL::to('/') }}/ajax/rm/vpurchase/{{$row->id}}">hapus</a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                    <tr>
+                                        <td>{{$no}}</td>
+                                        <td>{{$row->name}}</td>
+                                        <td>{{$row->ukuran}}</td>
+                                        <!--<td>{{number_format($row->stockist_price, 0, ',', ',')}}</td>-->
+                                        <td>{{number_format($row->member_price, 0, ',', ',')}}</td>
+                                        <td>{{$row->code}}</td>
+                                        <td>{{$row->area}}</td>
+                                        <td>
+                                            <a class="text-info"
+                                                href="{{ URL::to('/') }}/adm/edit/vpurchase/{{$row->id}}">edit</a>
+                                            &nbsp;&nbsp;
+                                            <a rel="tooltip" data-toggle="modal" data-target="#rmThis"
+                                                class="text-danger"
+                                                href="{{ URL::to('/') }}/ajax/rm/vpurchase/{{$row->id}}">hapus</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
                                     @endif
                                 </tbody>
                             </table>
-                             <div class="modal fade" id="rmThis" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="false">
+                            <div class="modal fade" id="rmThis" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="false">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content"></div>
                                 </div>
@@ -113,7 +119,7 @@
                 columnDefs: [
                     { orderable: false, targets: -1 }
                  ],
-                searching: false,
+                searching: true,
                  pagingType: "full_numbers",
                  "paging":   true,
                  "info":     false,
--- resources/views/member/digital/m_detail_vppob.blade.php
+++ resources/views/member/digital/m_detail_vppob.blade.php
@@ -22,7 +22,7 @@
                     <h6 class="mb-3">Keranjang Transaksi Member</h6>
 
                     @if ( Session::has('message') )
-                        <div class="container">
+                    <div class="container">
                         <div class="alert alert-{{ Session::get('messageclass') }} alert-dismissible" role="alert">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">×</span>
@@ -47,11 +47,11 @@
                                 </thead>
                                 <tbody>
                                     @if($getDataMaster != null)
-                                        <tr>
-                                            <td><b>{{$getDataMaster->message}}</td>
-                                            <td>{{number_format($getDataMaster->ppob_price, 0, ',', ',')}}</td>
-                                            <td>{{$getDataMaster->product_name}}</td>
-                                        </tr>
+                                    <tr>
+                                        <td><b>{{$getDataMaster->message}}</td>
+                                        <td>{{number_format($getDataMaster->ppob_price, 0, ',', ',')}}</td>
+                                        <td>{{$getDataMaster->product_name}}</td>
+                                    </tr>
                                     @endif
                                     <tr>
                                         <td>&nbsp;</td>
@@ -73,81 +73,98 @@
                                     <h5 class="label label-success">Tuntas</h5>
                                 </div>
                             </div>
-                            @if ($getDataMaster->type >=1 && $getDataMaster->type <3)
-                            <?php   $array = json_decode($getDataMaster->return_buy, true);?>
-                        <p class="card-text"><b>SN:</b> {{$array['data']['sn']}}</p>
-                            @endif
-                            @endif
-                            @if($getDataMaster->status == 3)
-                            <p class="card-text">Status</p>
-                            <div class="row" style="margin-bottom: 15px;">
-                                <div class="col-md-12">
-                                    <h5 class="label label-danger">Batal</h5>
-                                </div>
-                            </div>
-                            @endif
+                            @if ($getDataMaster->type >=1 && $getDataMaster->type <3 || $getDataMaster->type >=21 &&
+                                $getDataMaster->type <30)
+                                    <?php   $array = json_decode($getDataMaster->return_buy, true);?> <dd><b>SN:</b>
+                                    {{$array['data']['sn']}}</dd>
+                                    @endif
+                                    @endif
+                                    @if($getDataMaster->status == 3)
+                                    <p class="card-text">Status</p>
+                                    <div class="row" style="margin-bottom: 15px;">
+                                        <div class="col-md-12">
+                                            <h5 class="label label-danger">Batal</h5>
+                                        </div>
+                                    </div>
+                                    @endif
 
-                            <p class="card-text">Metode Pembayaran</p>
-                            <div class="row" style="margin-bottom: 15px;">
-                                <div class="col-md-12">
-                                    @if($getDataMaster->buy_metode == 1)
-                                    <div class="radio radio-primary" style="margin-bottom: 15px;">
-                                        <input type="radio" name="radio" id="radio1" value="1" checked="">
-                                        <label for="radio1">
-                                            COD
-                                        </label>
+                                    <p class="card-text">Metode Pembayaran</p>
+                                    <div class="row" style="margin-bottom: 15px;">
+                                        <div class="col-md-12">
+                                            @if($getDataMaster->buy_metode == 1)
+                                            <div class="radio radio-primary" style="margin-bottom: 15px;">
+                                                <input type="radio" name="radio" id="radio1" value="1" checked="">
+                                                <label for="radio1">
+                                                    COD
+                                                </label>
+                                            </div>
+                                            @endif
+                                            @if($getDataMaster->buy_metode == 3)
+                                            <div class="radio radio-primary" style="margin-bottom: 15px;">
+                                                <input type="radio" name="radio" id="radio3" value="3" checked="">
+                                                <label for="radio3">
+                                                    eIDR
+                                                </label>
+                                            </div>
+                                            @endif
+                                        </div>
                                     </div>
-                                    @endif
-                                    @if($getDataMaster->buy_metode == 3)
-                                    <div class="radio radio-primary" style="margin-bottom: 15px;">
-                                        <input type="radio" name="radio" id="radio3" value="3" checked="">
-                                        <label for="radio3">
-                                            eIDR
-                                        </label>
-                                    </div>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    @if($getDataMaster->buy_metode == 1)
-                                        @if($getDataMaster->status == 0)
-                                            <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
-                                            <button type="submit" class="btn btn-danger"  id="submitBtn" data-toggle="modal" data-target="#rejectSubmit" onClick="rejectSubmit()">Batal</button>
-                                        @endif
-                                        @if($getDataMaster->status == 1)
-                                            <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            @if($getDataMaster->buy_metode == 1)
+                                            @if($getDataMaster->status == 0)
+                                            <a class="btn btn-dark"
+                                                href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
+                                            <button type="submit" class="btn btn-danger" id="submitBtn"
+                                                data-toggle="modal" data-target="#rejectSubmit"
+                                                onClick="rejectSubmit()">Batal</button>
+                                            @endif
+                                            @if($getDataMaster->status == 1)
+                                            <a class="btn btn-dark"
+                                                href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
                                             @if($getDataMaster->vendor_cek == null)
-                                                @if($getDataMaster->return_buy == null)
-                                                <button type="submit" class="btn btn-danger"  id="submitBtn" data-toggle="modal" data-target="#rejectSubmit" onClick="rejectSubmit()">Batal</button>
-                                                &nbsp;
+                                            @if($getDataMaster->return_buy == null)
+                                            <button type="submit" class="btn btn-danger" id="submitBtn"
+                                                data-toggle="modal" data-target="#rejectSubmit"
+                                                onClick="rejectSubmit()">Batal</button>
+                                            &nbsp;
+                                            @endif
+                                            @endif
+                                            <button type="button" class="btn btn-success" id="submitBtn"
+                                                data-toggle="modal" data-target="#confirmSubmit"
+                                                onClick="inputSubmit()">Konfirmasi</button>
+                                            @endif
+                                            @if($getDataMaster->status == 2)
+                                            <a class="btn btn-dark"
+                                                href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
+                                            @if($getDataMaster->type >= 3 && $getDataMaster->type < 11) <a
+                                                class="btn btn-warning"
+                                                href="{{ URL::to('/') }}/m/vinvoice/ppob/{{$getDataMaster->id}}">Cetak
+                                                Struk
+                                                Transaksi</a>
                                                 @endif
-                                            @endif
-                                            <button type="button" class="btn btn-success"  id="submitBtn" data-toggle="modal" data-target="#confirmSubmit" onClick="inputSubmit()">Konfirmasi</button>
-                                        @endif
-                                        @if($getDataMaster->status == 2)
-                                            <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
-                                            @if($getDataMaster->type >= 3 && $getDataMaster->type < 11)
-                                            <a class="btn btn-warning" href="{{ URL::to('/') }}/m/vinvoice/ppob/{{$getDataMaster->id}}">Cetak Struk Transaksi</a>
-                                            @endif
-                                        @endif
-                                        @if($getDataMaster->status == 3)
-                                            <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
-                                        @endif
-                                    @endif
-                                    @if($getDataMaster->buy_metode == 3)
-                                    <a class="btn btn-dark" href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="modal fade" id="rejectSubmit" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="false">
-                                <div class="modal-dialog" role="document" id="rejectDetail">
-                                </div>
-                            </div>
-                            <div class="modal fade" id="confirmSubmit" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="false">
-                                <div class="modal-dialog" role="document" id="confirmDetail">
-                                </div>
-                            </div>
+                                                @endif
+                                                @if($getDataMaster->status == 3)
+                                                <a class="btn btn-dark"
+                                                    href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
+                                                @endif
+                                                @endif
+                                                @if($getDataMaster->buy_metode == 3)
+                                                <a class="btn btn-dark"
+                                                    href="{{ URL::to('/') }}/m/list/vppob-transaction">Kembali</a>
+                                                @endif
+                                        </div>
+                                    </div>
+                                    <div class="modal fade" id="rejectSubmit" tabindex="-1" role="dialog"
+                                        aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="false">
+                                        <div class="modal-dialog" role="document" id="rejectDetail">
+                                        </div>
+                                    </div>
+                                    <div class="modal fade" id="confirmSubmit" tabindex="-1" role="dialog"
+                                        aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="false">
+                                        <div class="modal-dialog" role="document" id="confirmDetail">
+                                        </div>
+                                    </div>
                         </div>
                     </div>
                 </div>
@@ -162,63 +179,66 @@
 
 
 @section('styles')
-    <link rel="stylesheet" href="{{ asset('asset_new/css/siderbar.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.9.95/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/fonts/slick.woff">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css">
+<link rel="stylesheet" href="{{ asset('asset_new/css/siderbar.css') }}">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.9.95/css/materialdesignicons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/fonts/slick.woff">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css">
 
 @stop
 
 @section('javascript')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="{{ asset('asset_new/js/sidebar.js') }}"></script>
-    @if($getDataMaster->buy_metode == 1)
-    @if($getDataMaster->status < 2)
-    <script>
-            $(document).ready(function(){
-                $('#show-password').click(function(){
-                    if($(this).is(':checked')){
-                            $('#2fa').attr('type','text');
-                    }else{
-                            $('#2fa').attr('type','password');
-                    }
-                });
-            });
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js">
+</script>
+<script src="{{ asset('asset_new/js/sidebar.js') }}"></script>
+@if($getDataMaster->buy_metode == 1)
+@if($getDataMaster->status < 2) <script>
 
-            function inputSubmit(){
-                 $.ajax({
-                     type: "GET",
-                     url: "{{ URL::to('/') }}/m/cek/confirm-2fa-ppob?id_ppob="+{{$getDataMaster->id}},
-                     success: function(url){
-                         $("#confirmDetail" ).empty();
-                         $("#confirmDetail").html(url);
-                     }
-                 });
-           }
+    function inputSubmit(){
+    $.ajax({
+    type: "GET",
+    url: "{{ URL::to('/') }}/m/cek/confirm-2fa-ppob?id_ppob="+{{$getDataMaster->id}},
+    success: function(url){
+    $("#confirmDetail" ).empty();
+    $("#confirmDetail").html(url);
+    setTimeout(() => {
+    $('#show-password').click(function(){
+    if($(this).is(':checked')){
+    $('#2fa').attr('type','text');
+    }else{
+    $('#2fa').attr('type','password');
+    }
+    });
+    }, 1000);
+    }
+    });
+    }
 
-           function rejectSubmit(){
-                 $.ajax({
-                     type: "GET",
-                     url: "{{ URL::to('/') }}/m/cek/reject/buy-ppob?id_ppob="+{{$getDataMaster->id}},
-                     success: function(url){
-                         $("#rejectDetail" ).empty();
-                         $("#rejectDetail").html(url);
-                     }
-                 });
-           }
+    function rejectSubmit(){
+    $.ajax({
+    type: "GET",
+    url: "{{ URL::to('/') }}/m/cek/reject/buy-ppob?id_ppob="+{{$getDataMaster->id}},
+    success: function(url){
+    $("#rejectDetail" ).empty();
+    $("#rejectDetail").html(url);
+    }
+    });
+    }
 
-            function confirmSubmit(){
-                var dataInput = $("#form-add").serializeArray();
-                $('#form-add').submit();
-                $('#form-add').remove();
-                $('#loading').show();
-                $('#tutupModal').remove();
-                $('#submit').remove();
-            }
+    function confirmSubmit(){
+    var dataInput = $("#form-add").serializeArray();
+    $('#form-add').submit();
+    $('#form-add').remove();
+    $('#loading').show();
+    $('#tutupModal').remove();
+    $('#submit').remove();
+    }
     </script>
     @endif
     @endif
-@stop
+    @stop
