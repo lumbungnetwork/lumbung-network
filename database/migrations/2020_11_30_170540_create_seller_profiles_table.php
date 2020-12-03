8d4b9a1b0c16d35eb085051b3a2bf6c292635f83
Evan Stinger <evanstinger@icloud.com>
2020-12-03 13:00:12 +0700

    Creating Category and Seller Migration

---
+++ database/migrations/2020_11_30_170540_create_seller_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellerProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('seller_id');
+            $table->string('shop_name', 25);
+            $table->string('motto', 65);
+            $table->string('no_hp', 16);
+            $table->string('tg_user', 40);
+            $table->string('image', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_profiles');
+    }
+}
