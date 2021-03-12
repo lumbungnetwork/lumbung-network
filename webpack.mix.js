4cf88e82068607a6830005c38e7371293a997f6e
Evan Stinger <evanstinger@icloud.com>
2021-03-13 01:00:38 +0700

    install & update tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .postCss("resources/css/app.css", "public/css", [
+      require("tailwindcss"),
+   ]);
+
+mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
