28f57e71fea5e1926de6a9fdc694ea91a535e11a
Evan Stinger <evanstinger@icloud.com>
2021-06-03 21:27:40 +0800

    fix history routing args

--- resources/views/member/app/wallet.blade.php
+++ resources/views/member/app/wallet.blade.php
@@ -41,7 +41,7 @@
                     @php
                     $href = "";
                     if ($row->source == 6) {
-                    $href = route('member.shopping.digitalPayment', ['masterSalesID' => $row->tx_id]);
+                    $href = route('member.shopping.digitalPayment', ['sale_id' => $row->tx_id]);
                     }
                     @endphp
 
