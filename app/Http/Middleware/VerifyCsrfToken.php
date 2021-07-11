6fc43f4603b949af395c0a2a661380b87896594d
Evan Stinger <evanstinger@icloud.com>
2021-07-11 23:06:12 +0700

    update VerifyCsrfToken to use wildcard for webhook

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/zMbH9dshaPZqdGIJtgvQNfsj38MfPRizcDuNeGu5xyvOWJaswzhkhFJaoeHddWaW/webhook',
+        '*/webhook',
         '/digiflazz/webhook',
     ];
 }
